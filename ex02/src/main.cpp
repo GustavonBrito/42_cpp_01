2834e92a999776b6f4670cb46732389ed242767e
GustavonBrito <gustavoserafio@gmail.com>
2026-03-10 08:35:09 -0300

    feat: how to instatiate heap and stack

---
+++ ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustavo-linux <gustavo-linux@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/06 13:29:32 by gustavo-lin       #+#    #+#             */
+/*   Updated: 2026/03/07 20:57:26 by gustavo-lin      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iostream"
+
+int main(void)
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR = &brain;
+    std::string &stringREF = brain;
+
+    std::cout << "Brain variable address: " << &brain << "\n";
+    std::cout << "Brain pointer: " << stringPTR << "\n";
+    std::cout << "Brain variable reference: " << &stringREF << "\n";
+    
+    std::cout << "Brain variable value: " << brain << "\n";
+    std::cout << "Brain pointer value: " << *stringPTR << "\n";
+    std::cout << "Brain reference value: " << stringREF << "\n";
+    
+    return 0;
+}
